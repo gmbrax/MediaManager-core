4ae1e3075df13bae2cf965199ad146a836773cc1
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2025-11-27 18:40:08 -0300

    Remove `Media` entity and integrate Hibernate ORM into `DatabaseManager`:
    - Deleted `Media` class and its annotations.
    - Added Hibernate ORM initialization in `DatabaseManager`, including dynamic entity scanning and configuration.
    - Updated `pom.xml` to include dependencies for Hibernate community dialects and Reflections.

--- src/main/java/com/mediamanager/service/database/DatabaseManager.java
+++ src/main/java/com/mediamanager/service/database/DatabaseManager.java
@@ -1,16 +1,26 @@
 package com.mediamanager.service.database;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+import org.hibernate.cfg.Configuration;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import jakarta.persistence.Entity;
+import java.util.Set;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+
 public abstract class DatabaseManager {
     protected final Properties configuration;
     protected Connection connection;
+    protected String connectionUrl;
+    protected EntityManagerFactory entityManagerFactory;
+    protected EntityManager entityManager;
     protected static final Logger logger = LogManager.getLogger(DatabaseManager.class);
 
     public DatabaseManager(Properties config) {
@@ -50,6 +60,7 @@ public abstract class DatabaseManager {
             logger.debug("No database connection to close");
         }
     }
+
     protected boolean testConnection() {
         try (Statement stmt = connection.createStatement()) {
             stmt.execute("SELECT 1");
@@ -58,5 +69,46 @@ public abstract class DatabaseManager {
             logger.error("Connection test failed", e);
             return false;
         }
+    }
+    protected void initializeHibernate() {
+        logger.info("Initializing Hibernate ORM...");
+
+        Configuration hibernateConfig = new Configuration();
+
+        // DEBUG PRIMEIRO - antes de usar as propriedades
+        String dialect = configuration.getProperty("hibernate.dialect");
+        String hbm2ddl = configuration.getProperty("hibernate.hbm2ddl.auto");
+        String driver = configuration.getProperty("database.driver");
+
+        logger.info("DEBUG - dialect: {}", dialect);
+        logger.info("DEBUG - hbm2ddl: {}", hbm2ddl);
+        logger.info("DEBUG - driver: {}", driver);
+        logger.info("DEBUG - connectionUrl: {}", connectionUrl);
+
+        // Agora usa as propriedades
+        hibernateConfig.setProperty("hibernate.connection.url", connectionUrl);
+        hibernateConfig.setProperty("hibernate.connection.driver_class", driver);
+        hibernateConfig.setProperty("hibernate.dialect", dialect);
+        hibernateConfig.setProperty("hibernate.hbm2ddl.auto", hbm2ddl);
+        hibernateConfig.setProperty("hibernate.show_sql",
+                configuration.getProperty("hibernate.show_sql", "false"));
+        hibernateConfig.setProperty("hibernate.format_sql",
+                configuration.getProperty("hibernate.format_sql", "true"));
+
+        logger.info("Scanning for entities in package: com.mediamanager.model");
+        Reflections reflections = new Reflections("com.mediamanager.model", Scanners.TypesAnnotated);
+        Set<Class<?>> entityClasses = reflections.getTypesAnnotatedWith(Entity.class);
+
+        logger.info("Found {} entities", entityClasses.size());
+        for (Class<?> entityClass : entityClasses) {
+            logger.debug("Registering entity: {}", entityClass.getSimpleName());
+            hibernateConfig.addAnnotatedClass(entityClass);
+        }
+
+        // Criar EntityManagerFactory
+        entityManagerFactory = hibernateConfig.buildSessionFactory().unwrap(EntityManagerFactory.class);
+        entityManager = entityManagerFactory.createEntityManager();
+
+        logger.info("Hibernate ORM initialized successfully");
     }
 }
